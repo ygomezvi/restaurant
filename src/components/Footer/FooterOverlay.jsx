fb87f112640ec889ce29f8b99cd50ffcc1939fa4
Yurimar Gomez <yurimar.gomez@davinci.edu.ar>
2024-11-19 21:42:58 -0300

    Agrego proyecto de restaurant Gericht

---
+++ src/components/Footer/FooterOverlay.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './FooterOverlay.css';
+
+const FooterOverlay = () => (
+  <div className='app__footerOverlay'>
+    Hola overlay
+    <div className='app__footerOverlay-black'></div>
+    <div className='app__footerOverlay-img app__bg'></div>
+  </div>
+);
+
+export default FooterOverlay;
---
+++ src/container/index.js
@@ -0,0 +1,17 @@
+import AboutUs from './AboutUs/AboutUs';
+import Chef from './Chef/Chef';
+import FindUs from './Findus/FindUs';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Intro from './Intro/Intro';
+import SpecialMenu from './Menu/SpecialMenu';
+
+export {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Header,
+  Intro,
+  SpecialMenu,
+};
